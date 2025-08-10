8fdf04fa09c2c630d6ee2794f5c765439fd7358d
Renzo Reyna <reynarenzo.88@gmail.com>
2025-08-10 17:55:29 -0300

    Nueva web Personal 2025

--- source/app.py
+++ source/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, jsonify
-from flask_mail import Mail
-from config import USUARIO_GMAIL, PASSWORD_GMAIL, SECRET_JWT, SECRET_KEY
-from flask_jwt_extended import JWTManager
-
+# from config import USUARIO_GMAIL, PASSWORD_GMAIL, SECRET_JWT, SECRET_KEY
 from routes.sitio import sitio
 
 
@@ -11,18 +8,18 @@ from routes.sitio import sitio
 app= Flask(__name__)
 
 
-app.config['MAIL_SERVER']= 'smtp.gmail.com'
-app.config['MAIL_PORT']= 465
-app.config['MAIL_USE_SSL']= True
-app.config['MAIL_USERNAME']= f'{USUARIO_GMAIL}'
-app.config['MAIL_PASSWORD']= f'{PASSWORD_GMAIL}'
-app.config['MAIL_DEFAULT_SENDER']= f'{USUARIO_GMAIL}'
-mail= Mail(app)
+# app.config['MAIL_SERVER']= 'smtp.gmail.com'
+# app.config['MAIL_PORT']= 465
+# app.config['MAIL_USE_SSL']= True
+# app.config['MAIL_USERNAME']= f'{USUARIO_GMAIL}'
+# app.config['MAIL_PASSWORD']= f'{PASSWORD_GMAIL}'
+# app.config['MAIL_DEFAULT_SENDER']= f'{USUARIO_GMAIL}'
+# mail= Mail(app)
 
-app.config['SECRET_KEY']= f'{SECRET_KEY}'
+# app.config['SECRET_KEY']= f'{SECRET_KEY}'
 
-app.config['JWT_SECRET_KEY']= f'{SECRET_JWT}'
-JWT= JWTManager(app)
+# app.config['JWT_SECRET_KEY']= f'{SECRET_JWT}'
+# JWT= JWTManager(app)
 
 app.register_blueprint(sitio)
 
@@ -36,7 +33,7 @@ def pagina_no_encontrada(error):
 
 
                                                               
-if __name__ == '__main__':
-    app.register_error_handler(404, pagina_no_encontrada)
-    app.register_error_handler(401, status_401)  
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.register_error_handler(404, pagina_no_encontrada)
+#     app.register_error_handler(401, status_401)  
+#     app.run(debug=True)
