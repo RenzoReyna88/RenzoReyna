da05dfc81dd55ee06ce4ca0d2c9b057ceb7f6696
Renzo Reyna <reynarenzo.88@gmail.com>
2025-08-11 16:21:42 -0300

    Se agregó funciones adicionales al archivo app.py

--- source/config.py
+++ source/config.py
@@ -3,18 +3,17 @@ import os
 load_dotenv()
 
 # datos para la conexión con la BD
-HOST_DB= os.environ['host']
-USER_DB= os.environ['user']
-PASSWORD_DB= os.environ['password']
-NAME_DB= os.environ['db']
+# HOST_DB= os.environ['host']
+# USER_DB= os.environ['user']
+# PASSWORD_DB= os.environ['password']
+# NAME_DB= os.environ['db']
 
 
 #datos gmail
 USUARIO_GMAIL= os.environ['user_gmail']
 PASSWORD_GMAIL= os.environ['password_gmail']  
-
-#jwt password
-SECRET_JWT= os.environ['jwt_key']
-
 #llave secreta
 SECRET_KEY= os.environ['secret_key']
+
+#jwt password
+# SECRET_JWT= os.environ['jwt_key']
